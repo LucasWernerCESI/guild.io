6ce334d61060301782a640610a4911d719338fff
Lucie <desmons.lucie@gmail.com>
2021-06-24 22:43:30 +0200

    [Implementation] applications --> readAll.php ok

--- Api/api/applications/readAll.php
+++ Api/api/applications/readAll.php
@@ -1,1 +1,54 @@
 <?php
+
+use Classes\Application;
+use Classes\Database;
+
+
+include_once('../../vendor/autoload.php');
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+
+try {
+
+    $item = new Application(Database::getConnection());
+
+
+    $stmt = $item->getAllApplications();
+    $itemCount = $stmt->rowCount();
+
+    var_dump($itemCount);
+
+    echo json_encode($itemCount);
+
+    if($itemCount > 0){
+
+        $applicationsArray = array();
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {  //fetch --> créer le tableau, récupère ligne par ligne
+
+            $e = array(
+                "id" => $row["id"],  //[liste]
+                "userId" => $row["userId"],
+                "guildId" => $row["guildId"],
+                "motivation" => $row["motivation"],
+                "PVP_or_PVE" => $row["PVP_or_PVE"],
+                "creationDate" => $row["creationDate"]
+            );
+
+            array_push($applicationsArray, $e); // stock les résultats dans un tableau
+        }
+
+        echo json_encode($applicationsArray);  //renvoie les données en json
+    } else {
+        http_response_code(404);
+        echo json_encode(
+            array("message" => "Candidature inexistante.")
+        );
+    }
+
+} catch (Exception $e) {
+}
+
+?>
